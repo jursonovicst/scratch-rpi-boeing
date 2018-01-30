aa49424366b3c26b23248ff739ae55f96f02e207
jursonovicst <github.com@jursonovics.com>
2018-01-30 22:44:34 +0100

    test

--- boeing-jsext/extension.js
+++ boeing-jsext/extension.js
@@ -1,4 +1,6 @@
 (function(ext) {
+    var mcp3008values = [-1,-1,-1,-1,-1,-1,-1,-1]
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,15 +10,30 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.power = function(spidev, mcp3008ch) {
+    ext.mcp3008getValue = function(spidev, ch) {
         return 5;
+    };
+
+    ext.when_mcp3008changes = function(spidev, ch) {
+        if (mcp3008ch[ch] == -1) {
+            mcp3008values[ch] = mcp3008getValue(spidev, ch);
+            return false;
+        }
+
+        if (mcp3008ch[ch] == mcp3008values[ch]) {
+            return false;
+        }
+
+        mcp3008values[ch] = mcp3008getValue(spidev, ch);
+        return true;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
-            ['r', 'read mcp3008 on SPI %m.spidev ch %m.mcp3008ch', 'power', 2, 3],
+            ['r', 'read mcp3008 on SPI %m.spidev ch %m.mcp3008ch', 'mcp3008getValue', 0, 0],
+            ['h', 'when mcp3008 on SPI %m.spidev ch %m.mcp3008ch changes', 'when_mcp3008changes', 0, 0]
         ],
         menus: {
             spidev: ['0','1'],
