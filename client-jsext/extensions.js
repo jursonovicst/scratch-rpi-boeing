29e00445db2bf127c2797f7dede8b07d95d28769
jursonovicst <github.com@jursonovics.com>
2018-05-30 23:21:37 +0200

    local extension descriptor

---
+++ client-jsext/extensions.js
@@ -0,0 +1,3 @@
+[
+    { "name":"RPi Boeing", "type":"extension", "file":"rpi-boeing-client-jsext.js", "md5":"rpi-boeing-thumbnail.png", "url":"https://jursonovicst.github.io/scratch-rpi-boeing", "tags":["hardware"]  }
+]
