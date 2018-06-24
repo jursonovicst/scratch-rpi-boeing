6f34303e67a64039465dad67b56fde215c0cd799
jursonovicst <github.com@jursonovics.com>
2018-06-24 23:59:18 +0200

    file url

--- client-jsext/extensions.js
+++ client-jsext/extensions.js
@@ -1,4 +1,4 @@
 [
-    { "name":"Cardplane HW",  "type":"extension", "file":"cardplane-hw-jsext.js",  "md5":"cardplane-hw-thumb.png",  "url":"https://jursonovicst.github.io/scratch-rpi-boeing", "tags":["hardware"]  },
-    { "name":"Cardplane API", "type":"extension", "file":"cardplane-api-jsext.js", "md5":"cardplane-api-thumb.png", "url":"https://jursonovicst.github.io/scratch-rpi-boeing", "tags":["api"]  }
+    { "name":"Cardplane HW",  "type":"extension", "file":"cardplane-hw-jsext.js",  "md5":"cardplane-hw-thumb.png",  "url":"file:///usr/lib/scratch2/scratch_extensions/cardplanehw.js",  "tags":["hardware"] },
+    { "name":"Cardplane API", "type":"extension", "file":"cardplane-api-jsext.js", "md5":"cardplane-api-thumb.png", "url":"file:///usr/lib/scratch2/scratch_extensions/cardplaneapi.js", "tags":["api"]      }
 ]
